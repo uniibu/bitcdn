4f5c102da41ffbd8dbf7020c13198a4881ac55e4
uni <unibtc@gmail.com>
2018-07-28 00:23:05 +0800

    chore:lint, add optimized feature

--- src/lib/belt.js
+++ src/lib/belt.js
@@ -3,19 +3,18 @@ const path = require('path');
 const sharp = require('sharp');
 const crypto = require('crypto');
 const needle = require('needle');
-
 const allowedFiles = {
-  image: {'png':{ext:'.png'},'jpeg':{ext:'.jpeg'},'jpg':{ext:'.jpg'},'gif':{ext:'.gif'},'x-icon':{ext:'.ico'},'svg+xml':{ext:'.svg'},'tiff':{ext:'.tiff'}, 'webp':{ext:'.webp'}},
-  application: {'javascript':{ext:'.js'},'json':{ext:'.json'},'xml':{ext:'.xml'}},
-  text: {'css':{ext:'.css'}}
+  image: { 'png': { ext: '.png' }, 'jpeg': { ext: '.jpeg' }, 'jpg': { ext: '.jpg' }, 'gif': { ext: '.gif' }, 'x-icon': { ext: '.ico' }, 'svg+xml': { ext: '.svg' }, 'tiff': { ext: '.tiff' }, 'webp': { ext: '.webp' } },
+  application: { 'javascript': { ext: '.js' }, 'json': { ext: '.json' }, 'xml': { ext: '.xml' } },
+  text: { 'css': { ext: '.css' } }
 };
 const sharpOpts = {
-  jpeg: {quality:70},
-  png: {progressive: false},
-  webp:{quality:70,lossless: true }
+  jpeg: { quality: 70 },
+  png: { progressive: false },
+  webp: { quality: 70, lossless: true }
 };
 
-function isBase64 (str) {
+function isBase64(str) {
   const notBase64 = /[^A-Z0-9+/=]/i;
   const len = str.length;
   if (!len || len % 4 !== 0 || notBase64.test(str)) {
@@ -23,21 +22,24 @@ function isBase64 (str) {
   }
   const firstPaddingChar = str.indexOf('=');
   return (
-    firstPaddingChar === -1 || firstPaddingChar === len - 1 || (firstPaddingChar === len - 2 && str[len - 1] === '=')
+    firstPaddingChar === -1 || firstPaddingChar === len - 1 || (firstPaddingChar === len - 2 &&
+      str[len - 1] === '=')
   );
 }
-function generateRand(size=3){
+
+function generateRand(size = 3) {
   return crypto.randomBytes(size).toString('hex');
 }
-function formatPath(type, fpath, data){
+
+function formatPath(type, fpath, data) {
   const pathObj = path.parse(fpath);
-  if(type == 'name'){
+  if (type == 'name') {
     return path.format({
       dir: pathObj.dir,
       name: `${pathObj.name}-${data}`,
       ext: pathObj.ext
     });
-  }else if(type == 'ext'){
+  } else if (type == 'ext') {
     return path.format({
       dir: pathObj.dir,
       name: pathObj.name,
@@ -45,72 +47,77 @@ function formatPath(type, fpath, data){
     });
   }
 }
-
-const validateFile = exports.validateFile = async (dataUri, dir, filepath, maxSize=1024,convert=false) => {
+const validateFile = exports.validateFile = async (dataUri, dir, filepath, maxSize = 1024, convert =
+false, optimized) => {
   const parts = dataUri.split(',');
   if (parts.length < 2 || parts.length > 2) {
     throw new Error('Invalid data');
-  } 
+  }
   const parsedData = dataUri.match(/^data:(.+);base64,(.+)$/);
   const base64Data = parsedData[2];
   const fileType = parsedData[1].split('/');
   const allowed = allowedFiles[fileType[0]];
-  if(!allowed || !Object.keys(allowed).includes(fileType[1]) || !isBase64(base64Data)){
+  if (!allowed || !Object.keys(allowed).includes(fileType[1]) || !isBase64(base64Data)) {
     throw new Error('Invalid data');
   }
   const fileExt = allowed[fileType[1]].ext;
   const filepathExt = path.extname(filepath);
-  if(filepathExt !== fileExt){
-    filepath = formatPath('ext',filepath,fileExt);    
+  if (filepathExt !== fileExt) {
+    filepath = formatPath('ext', filepath, fileExt);
   }
   const base64Buff = Buffer.from(base64Data, 'base64');
-  if(!base64Buff){
+  if (!base64Buff) {
     throw new Error('Invalid data');
   }
   const inKb = Buffer.byteLength(base64Buff) / 1024;
-  if(inKb > maxSize){
+  if (inKb > maxSize) {
     throw new Error('File size exceeded 1024 kb');
   }
-  
-  try{
-    let relativePath = path.join(dir,filepath);
-    if(fs.existsSync(relativePath)){
-      relativePath = formatPath('name',relativePath,generateRand());
+  try {
+    let relativePath = path.join(dir, filepath);
+    let response = [];
+    if (fs.existsSync(relativePath)) {
+      relativePath = formatPath('name', relativePath, generateRand());
     }
-    if(fileType[0] == 'image' && ['.jpg','.jpeg','.png','.webp'].includes(fileExt)){
+    if (fileType[0] == 'image' && ['.jpg', '.jpeg', '.png', '.webp'].includes(fileExt)) {
       let sharpImg = sharp(base64Buff);
-      convert = convert || fileType[1];
-      if(convert){
-        convert = convert == 'jpg' ? 'jpeg':convert;
-        relativePath = formatPath('ext',relativePath,`.${convert}`);
-        sharpImg = sharpImg[convert](sharpOpts[convert]);
+      convert = [convert] || [fileType[1]];
+      if (optimized) {
+        convert.push('webp');
       }
-   
-      await fs.ensureDir(path.dirname(relativePath));
-      await sharpImg.toFile(relativePath);        
-    }else{
-      await fs.outputFile(relativePath,base64Buff);
+      for (let cv of convert) {
+        if (cv) {
+          cv = cv == 'jpg' ? 'jpeg' : cv;
+          relativePath = formatPath('ext', relativePath, `.${cv}`);
+          sharpImg = sharpImg[cv](sharpOpts[cv]);
+        }
+        await fs.ensureDir(path.dirname(relativePath));
+        await sharpImg.toFile(relativePath);
+        response.push(path.join(path.dirname(filepath), path.basename(relativePath)));
+      }
+    } else {
+      await fs.outputFile(relativePath, base64Buff);
+      response.push(path.join(path.dirname(filepath), path.basename(relativePath)));
     }
-    return path.join(path.dirname(filepath),path.basename(relativePath));
-  }catch(e){
+    return response.length == 1 ? response[0] : response;
+  } catch (e) {
     console.log(e.message);
     throw new Error('Error occured while saving your file');
   }
 };
-
-exports.processFetchUrl = async (imageUrl,dir,convert=false) => {
-  try{
-    const resp = await needle('get',imageUrl);
+exports.processFetchUrl = async (imageUrl, dir, convert = false, optimized) => {
+  try {
+    const resp = await needle('get', imageUrl);
     const sharpImg = sharp(resp.body);
-    const {format} = await sharpImg.metadata();
-    if (!['png','jpeg','jpg','webp'].includes(format)) {
+    const { format } = await sharpImg.metadata();
+    if (!['png', 'jpeg', 'jpg', 'webp'].includes(format)) {
       return false;
     }
     const buff = await sharpImg.toBuffer();
     const dataUri = `data:image/${format};base64,${buff.toString('base64')}`;
-    const filepath = `/gallery/${generateRand(5)}`;  
-    return await validateFile(dataUri, dir, filepath,1024,convert); 
-  }catch(e){
+    const filepath = `/gallery/${generateRand(5)}`;
+    return await validateFile(dataUri, dir, filepath, 1024, convert, optimized);
+  } catch (e) {
     console.log(e.message);
     throw new Error('Error fetching url, must be a valid image file');
   }
--- src/routes/private.js
+++ src/routes/private.js
@@ -1,29 +1,52 @@
 const belt = require('../lib/belt');
 const config = require('../config');
 const path = require('path');
-const rootPath = path.resolve(__dirname,'../../..');
+const rootPath = path.resolve(__dirname, '../../..');
 module.exports = router => {
   router.post('/upload', async (ctx) => {
     ctx.validateBody('data').required('Invalid/Missing Data').isString('Invalid/Missing Data');
-    ctx.validateBody('filepath').required('Invalid/Missing Filepath').isString('Invalid/Missing Filepath')
-      .tap(f => path.normalize(`/${f.replace(/(\.){2,}(\/)/g,'')}`));
-    ctx.validateQuery('convert').optional().isIn(['jpg','jpeg','png','webp'],'Allowed convert parameters jpg,jpeg,png,webp');
-    try{
-      const valid = await belt.validateFile(ctx.vals.data, path.join(rootPath,ctx.state.userinfo.dir), ctx.vals.filepath,1024,ctx.vals.convert);
-      ctx.check(valid,'Invalid/Missing File');
-      ctx.ok(`https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid}`);
-    }catch(e){
+    ctx.validateBody('filepath').required('Invalid/Missing Filepath').isString(
+      'Invalid/Missing Filepath')
+      .tap(f => path.normalize(`/${f.replace(/(\.){2,}(\/)/g, '')}`));
+    ctx.validateQuery('convert').optional().isIn(['jpg', 'jpeg', 'png', 'webp'],
+      'Allowed convert parameters jpg,jpeg,png,webp');
+    ctx.validateQuery('optimized').optional().isString('Invalid Query');
+    try {
+      const uinfoDir = ctx.state.userinfo.dir;
+      const valid = await belt.validateFile(ctx.vals.data, path.join(rootPath, uinfoDir),
+        ctx.vals.filepath, 1024, ctx.vals.convert, ctx.vals.optimized);
+      ctx.check(valid, 'Invalid/Missing File');
+      if (ctx.vals.optimized && Array.isArray(valid)) {
+        ctx.ok({
+          original: `https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid[0]}`,
+          optmized: `https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid[1]}`
+        });
+      } else {
+        ctx.ok(`https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid}`);
+      }
+    } catch (e) {
       ctx.fail(e.message);
-    }    
+    }
   });
-  router.post('/uploadurl',async ctx => {
+  router.post('/uploadurl', async ctx => {
     ctx.validateBody('url').required('Invalid/Missing Url').isString('Invalid/Missing Url');
-    ctx.validateQuery('convert').optional().isIn(['jpg','jpeg','png','webp'],'Allowed convert parameters jpg,jpeg,png,webp');
-    try{
-      const valid = await belt.processFetchUrl(ctx.vals.url,path.join(rootPath,ctx.state.userinfo.dir),ctx.vals.convert);
-      ctx.check(valid,'Invalid/Missing File');
-      ctx.ok(`https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid}`);
-    }catch(e){
+    ctx.validateQuery('convert').optional().isIn(['jpg', 'jpeg', 'png', 'webp'],
+      'Allowed convert parameters jpg,jpeg,png,webp');
+    ctx.validateQuery('optimized').optional().isString('Invalid Query');
+    try {
+      const uinfoDir = ctx.state.userinfo.dir;
+      const valid = await belt.processFetchUrl(ctx.vals.url, path.join(rootPath, uinfoDir),
+        ctx.vals.convert, ctx.vals.optimized);
+      ctx.check(valid, 'Invalid/Missing File');
+      if (ctx.vals.optimized && Array.isArray(valid)) {
+        ctx.ok({
+          original: `https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid[0]}`,
+          optmized: `https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid[1]}`
+        });
+      } else {
+        ctx.ok(`https://${ctx.state.userinfo.suburl}.${config.APIURL}${valid}`);
+      }
+    } catch (e) {
       ctx.fail(e.message);
     }
   });
